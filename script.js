d9819ccec0f91f5e34c335cc62bcf34f26cffc6c
gsincodes <gsincodes@gmail.com>
2024-04-06 15:45:42 +0530

    add playRound Function

--- script.js
+++ script.js
@@ -11,12 +11,53 @@ function getComputerChoice(){
     }
 }
 
-let computerSelection = getComputerChoice();
-
-let playerSelection = function(){
+function getPlayerChoice(){
     let playerMove = prompt("Enter your move: ");
     return playerMove.toLowerCase();
 };
 
-console.log(`Computer Move -> ${computerSelection} 
-playerSelection -> ${playerSelection()}`);
+function playRound(playerSelection, computerSelection){
+
+    //playerSelection -> rock
+
+    if(playerSelection === 'rock' && computerSelection === 'rock'){
+        return 'Tie';
+    }
+    else if(playerSelection === 'rock' && computerSelection === 'paper'){
+        return 'You Won';
+    }
+    else if(playerSelection === 'rock' && computerSelection === 'scissor'){
+        return 'You Lose';
+    }
+
+    //playerSelection -> paper
+
+    else if(playerSelection === 'paper' && computerSelection === 'rock'){
+        return 'You Won';
+    }
+    else if(playerSelection === 'paper' && computerSelection === 'paper'){
+        return 'Tie';
+    }
+    else if(playerSelection === 'paper' && computerSelection === 'scissor'){
+        return 'You Lose';
+    }
+
+    //playerSelection -> scissor
+
+    else if(playerSelection === 'scissor' && computerSelection === 'rock'){
+        return 'You Lose';
+    }
+    else if(playerSelection === 'scissor' && computerSelection === 'paper'){
+        return 'You Won';
+    }
+    else if(playerSelection === 'scissor' && computerSelection === 'scissor'){
+        return 'Tie';
+    }
+
+}
+
+const computerSelection = getComputerChoice();
+const playerSelection = getPlayerChoice();
+
+
+console.log(playRound(playerSelection,computerSelection));
