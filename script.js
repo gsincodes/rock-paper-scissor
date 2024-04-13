0d8350b7cf6aeb23d4587c3e19af5cca3b042d42
gsincodes <gsincodes@gmail.com>
2024-04-13 15:51:05 +0530

    add rock paper scissor UI and functinalities

--- script.js
+++ script.js
@@ -1,3 +1,34 @@
+// rock paper scissor click Event Listener
+
+let rock = document.getElementById('rock');
+rock.addEventListener('click',() => {
+    playerSelection = 'rock';
+    playRound(playerSelection,getComputerChoice());
+});
+
+let paper = document.getElementById('paper');
+paper.addEventListener('click', () =>{
+    playerSelection = 'paper';
+    playRound(playerSelection,getComputerChoice());
+
+});
+
+let scissor = document.getElementById('scissor');
+scissor.addEventListener('click',() => {
+    playerSelection = 'scissor';
+    playRound(playerSelection,getComputerChoice());
+
+})
+
+//some variables
+
+let div = document.querySelector('div');
+let playerScore = 0;
+let compScore = 0;
+let round = 1;
+
+//computer choice generator
+
 function getComputerChoice(){
     let num = Math.random() * 100;
     if(num<33.33){
@@ -11,59 +42,111 @@ function getComputerChoice(){
     }
 }
 
-function getPlayerChoice(){
-    let playerMove = prompt("Enter your move: ");
-    return playerMove.toLowerCase();
-};
+//play round function
 
-function playGame(){
-    let i = 1;
-    while(i <= 5){
-        function playRound(playerSelection, computerSelection){
+function playRound(playerSelection, computerSelection){
 
-            //playerSelection -> rock
-        
-            if(playerSelection === 'rock' && computerSelection === 'rock'){
-                return 'Tie';
-            }
-            else if(playerSelection === 'rock' && computerSelection === 'paper'){
-                return 'You Won';
-            }
-            else if(playerSelection === 'rock' && computerSelection === 'scissor'){
-                return 'You Lose';
-            }
-        
-            //playerSelection -> paper
-        
-            else if(playerSelection === 'paper' && computerSelection === 'rock'){
-                return 'You Won';
-            }
-            else if(playerSelection === 'paper' && computerSelection === 'paper'){
-                return 'Tie';
-            }
-            else if(playerSelection === 'paper' && computerSelection === 'scissor'){
-                return 'You Lose';
-            }
-        
-            //playerSelection -> scissor
-        
-            else if(playerSelection === 'scissor' && computerSelection === 'rock'){
-                return 'You Lose';
-            }
-            else if(playerSelection === 'scissor' && computerSelection === 'paper'){
-                return 'You Won';
-            }
-            else if(playerSelection === 'scissor' && computerSelection === 'scissor'){
-                return 'Tie';
-            }
-        
-        }
-        const computerSelection = getComputerChoice();
-        const playerSelection = getPlayerChoice();
-        console.log(`Round ${i} -> ${playRound(playerSelection,computerSelection)}`);
-        i++;
+    let result = '';
+
+    //playerSelection -> rock
+
+    if(playerSelection === 'rock' && computerSelection === 'rock'){
+        result = 'Tie';
+    }
+    else if(playerSelection === 'rock' && computerSelection === 'paper'){
+        result = 'You Lose';
+    }
+    else if(playerSelection === 'rock' && computerSelection === 'scissor'){
+        result = 'You Won';
     }
 
-}
+    //playerSelection -> paper
 
-playGame();
+    else if(playerSelection === 'paper' && computerSelection === 'rock'){
+        result = 'You Won';
+    }
+    else if(playerSelection === 'paper' && computerSelection === 'paper'){
+        result = 'Tie';
+    }
+    else if(playerSelection === 'paper' && computerSelection === 'scissor'){
+        result = 'You Lose';
+    }
+
+    //playerSelection -> scissor
+
+    else if(playerSelection === 'scissor' && computerSelection === 'rock'){
+        result = 'You Lose';
+    }
+    else if(playerSelection === 'scissor' && computerSelection === 'paper'){
+        result = 'You Won';
+    }
+    else if(playerSelection === 'scissor' && computerSelection === 'scissor'){
+        result = 'Tie';
+    }
+
+    //New Game Announcement
+
+    if(playerScore == 0 && compScore == 0 && round == 1){
+        let gameStart = document.createElement('h1');
+        gameStart.textContent = 'New Game';
+        div.appendChild(gameStart);
+    }
+
+    // Tracks Score
+
+    if(result == 'You Won'){
+        playerScore++;
+    }
+    else if(result == 'You Lose'){
+        compScore++;
+    }
+
+    // Displays Rounds, player Selection, comp Selection and Scores
+
+    let heading = document.createElement('h2');
+    heading.textContent = `Round ${round++}`;
+    div.appendChild(heading);
+
+    let compMove = document.createElement('p');
+    compMove.textContent = `Computer Selection => ${computerSelection}`;
+    div.appendChild(compMove);
+
+    let playerMove = document.createElement('p');
+    playerMove.textContent = `Player Selection => ${playerSelection}`;
+    div.appendChild(playerMove);
+
+    let roundResult = document.createElement('p');
+    roundResult.textContent = result;
+    div.appendChild(roundResult);
+
+    let scoreBoard = document.createElement('h3');
+    scoreBoard.textContent = `Player Score => ${playerScore} Computer Score => ${compScore}`;
+    div.appendChild(scoreBoard);
+
+    // Declares results 
+
+    if(playerScore == 5 && compScore<5){
+        let finalResult = document.createElement('h1');
+        finalResult.textContent = 'Victory'
+        div.appendChild(finalResult);
+        alert('Victory');
+    }
+    else if(compScore==5 && playerScore<5){
+        let finalResult = document.createElement('h1');
+        finalResult.textContent = 'Defeat'
+        div.appendChild(finalResult);
+        alert('Defeat');
+    }
+    
+    //Game Over Announcement
+
+    if(playerScore >= 5 || compScore >= 5){
+        playerScore = 0;
+        compScore = 0;
+        round = 1;
+        let gameOver = document.createElement('h1');
+        gameOver.textContent = 'Game Ended';
+        div.appendChild(gameOver);   
+    }
+    
+}
